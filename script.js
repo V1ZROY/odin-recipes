299a6f2044a9486195dc3eff54d8be682b963275
V1ZROY <vizoronah@googlemail.com>
2022-11-06 15:34:21 +0000

    Add home button to navigation to go to index

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const hidden = document.querySelectorAll(".hidden");
 //new variable to select element I want observed 
 //querySelectorAll creates an array of all elements with that css selector
-const options = {}
-//this variable is options for the Intersection observer
+const options = {rootMargin: "-100px"}
+//this variable is optgit lohions for the Intersection observer
 const observer = new IntersectionObserver(
     entries => {//function passed into intersection observer
     entries.forEach(entry => {
